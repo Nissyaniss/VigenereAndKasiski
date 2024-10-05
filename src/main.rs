f6e711c4804e93c2061df7a3d0058d1afa027983
Yaniss Lasbordes <yaniss.lasbordes@gmail.com>
2024-10-05 14:18:24 +0200

    fixed time string

--- src/main.rs
+++ src/main.rs
@@ -48,8 +48,7 @@ fn main() {
 			let text = fs::read_to_string(file).expect("Unable to read file");
 			let result = kasiski::find_key_length(&text, min_pair_length);
 			if result == 1 || result == 0 || result == 2 {
-				println!("The key is either too short for practical use or my calculations are just wrong.
-					I can recommend using another minimal key length because more lengthy texts tend to need more for the length of the key to be found.");
+				println!("The key is either too short (0, 1, 2) for practical use or my calculations are just wrong.\nI can recommend using another minimal key length because more lengthy texts tend to need more for the length of the key to be found.");
 			} else {
 				println!("The key length of the provided text is {result}");
 			}
@@ -57,7 +56,7 @@ fn main() {
 	}
 	let elapsed_time = now.elapsed();
 	println!(
-		"The finding took {}.{}{}s ",
+		"The program took {}.{}{}s ",
 		elapsed_time.as_secs(),
 		elapsed_time.as_millis() % 1000,
 		elapsed_time.as_micros() % 1000,
